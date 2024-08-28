4ee6e76c7b3ba44e7ab826ddda9fb2b492ad86c8
Gitmohammede <126097816+Gitmohammede@users.noreply.github.com>
2024-08-28 16:22:34 +0100

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-try:
-    from transformers import AutoModelForCausalLM, AutoTokenizer
-    print("Les bibliothèques transformers et torch sont importées correctement.")
-except ImportError as e:
-    print("Erreur d'importation:", e)
